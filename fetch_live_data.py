447f2877c0a2b3badb8f5a6d39b40ba193c33457
Prayash Dash <prayash.dash.eee21@itbhu.ac.in>
2024-09-14 16:45:54 +0530

    fixed model path error

--- fetch_live_data.py
+++ fetch_live_data.py
@@ -6,20 +6,20 @@ import streamlit as st
 
 class Data:
     def __init__(self):
-        self.base_url = "https://" + st.secrets["API_HOST"]
-        self.headers = {
-            "X-RapidAPI-Key": st.secrets["API_KEY"],
-            "X-RapidAPI-Host": st.secrets["API_HOST"]
-        }
-        self.series_id = st.secrets["SERIES_ID"]
-
-        # self.config = dotenv_values(".env")
-        # self.base_url = "https://" + self.config["API_HOST"]
+        # self.base_url = "https://" + st.secrets["API_HOST"]
         # self.headers = {
-        #     "X-RapidAPI-Key": self.config["API_KEY"],
-        #     "X-RapidAPI-Host": self.config["API_HOST"]
+        #     "X-RapidAPI-Key": st.secrets["API_KEY"],
+        #     "X-RapidAPI-Host": st.secrets["API_HOST"]
         # }
-        # self.series_id = self.config["SERIES_ID"]
+        # self.series_id = st.secrets["SERIES_ID"]
+
+        self.config = dotenv_values(".env")
+        self.base_url = "https://" + self.config["API_HOST"]
+        self.headers = {
+            "X-RapidAPI-Key": self.config["API_KEY"],
+            "X-RapidAPI-Host": self.config["API_HOST"]
+        }
+        self.series_id = self.config["SERIES_ID"]
 
     def get_matches(self):
         url = self.base_url + "/fixtures-by-series/" + str(self.series_id)
